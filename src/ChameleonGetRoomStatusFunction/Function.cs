7b5fea9dcfe52e0a6f3eb56ae88c45f3d9d4220b
Jacob Lever <jacoblever@gmail.com>
2019-07-11 00:01:26 +0100

    Integrate DynamoDB

--- src/ChameleonGetRoomStatusFunction/Function.cs
+++ src/ChameleonGetRoomStatusFunction/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using DataStore;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -14,7 +15,7 @@ namespace ChameleonGetRoomStatusFunction
                 peopleCount: 4,
                 chameleonCount: 2,
                 state: RoomState.InGame,
-                character: "butterfly");
+                character: Client.TestGet());
         }
     }
 }
--- src/ChameleonJoinRoomFunction/Function.cs
+++ src/ChameleonJoinRoomFunction/Function.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using DataStore;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -10,12 +11,19 @@ namespace ChameleonJoinRoomFunction
         public Person FunctionHandler(string input, ILambdaContext context)
         {
             var value = input?.ToUpper();
-            return new Person();
+            var a = Client.TestSave();
+            return new Person(roomCode: a, personId: "Person1");
         }
     }
 
     public class Person
     {
+        public Person(string roomCode, string personId)
+        {
+            RoomCode = roomCode;
+            PersonId = personId;
+        }
+
         public string RoomCode { get; }
         public string PersonId { get; }
     }
