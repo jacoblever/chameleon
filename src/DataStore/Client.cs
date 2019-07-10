681222b40dfc1f5d3679c6de088f5113c1211a94
Jacob Lever <jacoblever@gmail.com>
2019-07-11 00:01:26 +0100

    Integrate DynamoDB

---
+++ src/DataStore/Client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DocumentModel;
+
+namespace DataStore
+{
+    public class Client
+    {
+        public static string TestSave()
+        {
+            try
+            {
+                var client = CreateClient();
+                var table = Table.LoadTable(client, "ChameleonData");
+                Document doc = Document.FromJson("{\"RoomCode\": \"ABCD\", \"test\": \"Hello Jacob\"}");
+                Task putItem = table.PutItemAsync(doc);
+                putItem.Wait();
+                return "Done!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public static string TestGet()
+        {
+            try
+            {
+                var client = CreateClient();
+                var table = Table.LoadTable(client, "ChameleonData");
+                var getTask = table.GetItemAsync("ABCD");
+                getTask.Wait();
+                var doc = getTask.Result;
+                return doc["test"].AsString();
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        private static AmazonDynamoDBClient CreateClient()
+        {
+            AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+            clientConfig.RegionEndpoint = RegionEndpoint.EUWest1;
+            return new AmazonDynamoDBClient();
+        }
+    }
+}
---
+++ test/DataStore.Tests/DocumentTests.cs
@@ -0,0 +1,15 @@
+using Amazon.DynamoDBv2.DocumentModel;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class DocumentTests
+    {
+        [Test]
+        public void TestCanGetValueFromDocument()
+        {
+			Document doc = Document.FromJson("{\"RoomCode\": \"ABCD\", \"test\": \"Hello Jacob\"}");
+			Assert.That(doc["test"].AsString(), Is.EqualTo("Hello Jacob"));
+        }
+    }
+}
