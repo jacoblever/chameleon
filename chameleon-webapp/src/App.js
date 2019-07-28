d0f1a8eaed575548dcb0e4807cd4c3e526395485
Jacob Lever <jacobianism@gmail.com>
2019-07-28 19:55:16 +0100

    Make webapp change component once we have joined a room

--- chameleon-webapp/src/App.js
+++ chameleon-webapp/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class JoinRoomComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {roomCode: ''};
+    this.state = { roomCode: '' };
 
     this.handleChange = this.handleChange.bind(this);
     this.createRoom = this.createRoom.bind(this);
     this.joinRoom = this.joinRoom.bind(this);
+    this.makeJoinRoomRequest = this.makeJoinRoomRequest.bind(this);
   }
 
   createRoom(e) {
@@ -21,7 +21,7 @@ class JoinRoomComponent extends React.Component {
   }
 
   makeJoinRoomRequest(roomCode) {
-    fetch('https://chameleon.jacoblever.dev/api/join-room/', {
+    fetch(process.env.REACT_APP_CHAMELEON_BACKEND_BASE_URL + '/api/join-room/', {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -31,6 +31,9 @@ class JoinRoomComponent extends React.Component {
         roomCode: roomCode,
       })
     })
+      .then(response => response.json())
+      .then(jsonBody => this.props.onRoomJoined(jsonBody.RoomCode, jsonBody.PersonId))
+      .catch((error) => { console.error(error); })
   }
 
   handleChange(event) {
@@ -49,20 +52,54 @@ class JoinRoomComponent extends React.Component {
   }
 }
 
+class InRoomComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        Welcome {this.props.personId} to room {this.props.roomCode}
+      </div>
+    );
+  }
+}
+
+class ChameleonComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomCode: null,
+      personId: null
+    };
+    
+    this.onRoomJoined = this.onRoomJoined.bind(this);
+  }
+
+  onRoomJoined(roomCode, personId) {
+    this.setState({roomCode: roomCode, personId: personId});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Welcome to Chameleon!
+          </p>
+          <img
+            src='https://image.shutterstock.com/image-vector/cartoon-lizard-on-branch-260nw-370250969.jpg'
+            alt="Chameleon!"
+          />
+          <br />
+          {(this.state.roomCode != null) ? <InRoomComponent roomCode={this.state.roomCode} personId={this.state.personId} /> : <JoinRoomComponent onRoomJoined={this.onRoomJoined} />}
+        </header>
+      </div>
+    );
+  }
+}
 
 function App() {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to Chameleon!
-        </p>
-        <img src='https://image.shutterstock.com/image-vector/cartoon-lizard-on-branch-260nw-370250969.jpg'/>
-        <br/>
-        <JoinRoomComponent></JoinRoomComponent>
-      </header>
-    </div>
+    <ChameleonComponent></ChameleonComponent>
   );
 }
 
