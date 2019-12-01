317cd179beab9973949fbe3e2fabaa436fd1510f
Anita Nandi <anita.k.nandi@gmail.com>
2019-12-01 17:34:22 +0000

    Save cookies for roomCode and personId, still in room when page is refreshed

--- chameleon-webapp/src/components/ChameleonComponent.js
+++ chameleon-webapp/src/components/ChameleonComponent.js
@@ -9,12 +9,39 @@ class ChameleonComponent extends React.Component {
       roomCode: null,
       personId: null
     };
-    
+
+    var roomCode = this.readCookie("roomCode");
+    var personId = this.readCookie("personId");
+    if(roomCode) {
+      this.state.roomCode = roomCode;
+    }
+    if(personId) {
+      this.state.personId = personId;
+    }
+
     this.onRoomJoined = this.onRoomJoined.bind(this);
   }
 
   onRoomJoined(roomCode, personId) {
+    document.cookie = "personId=" + personId;
+    document.cookie = "roomCode=" + roomCode;
     this.setState({roomCode: roomCode, personId: personId});
+  }
+
+  readCookie(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+    for(var i = 0; i < cookieArray.length; i++) {
+      var cookieString = cookieArray[i];
+      while (cookieString.charAt(0) === ' ') {
+        cookieString = cookieString.substring(1);
+      }
+      if (cookieString.indexOf(name) === 0) {
+        return cookieString.substring(name.length, cookieString.length);
+      }
+    }
+    return null;
   }
 
   render() {
