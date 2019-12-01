7d4146c963f93900cbdcd6847e87cd0271ef9b09
Anita Nandi <anita.k.nandi@gmail.com>
2019-12-01 20:01:19 +0000

    Import js-cookie package to set and get cookies. Makes code tidier

--- chameleon-webapp/src/components/ChameleonComponent.js
+++ chameleon-webapp/src/components/ChameleonComponent.js
@@ -1,49 +1,25 @@
 import React from 'react';
 import JoinRoomComponent from './JoinRoomComponent';
 import InRoomComponent from './InRoomComponent';
+import Cookies from 'js-cookie';
 
 class ChameleonComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      roomCode: null,
-      personId: null
+      roomCode: Cookies.get('roomCode'),
+      personId: Cookies.get('personId')
     };
-
-    var roomCode = this.readCookie("roomCode");
-    var personId = this.readCookie("personId");
-    if(roomCode) {
-      this.state.roomCode = roomCode;
-    }
-    if(personId) {
-      this.state.personId = personId;
-    }
 
     this.onRoomJoined = this.onRoomJoined.bind(this);
   }
 
   onRoomJoined(roomCode, personId) {
-    document.cookie = "personId=" + personId;
-    document.cookie = "roomCode=" + roomCode;
+    Cookies.set('personId', personId);
+    Cookies.set('roomCode', roomCode);
     this.setState({roomCode: roomCode, personId: personId});
   }
-
-  readCookie(cookieName) {
-    var name = cookieName + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var cookieArray = decodedCookie.split(';');
-    for(var i = 0; i < cookieArray.length; i++) {
-      var cookieString = cookieArray[i];
-      while (cookieString.charAt(0) === ' ') {
-        cookieString = cookieString.substring(1);
-      }
-      if (cookieString.indexOf(name) === 0) {
-        return cookieString.substring(name.length, cookieString.length);
-      }
-    }
-    return null;
-  }
-
+  
   render() {
     return (
       <div className="App">
