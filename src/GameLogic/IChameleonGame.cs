192b613715a69d1606f9a07fc813f182b0cb91dc
Anita Nandi <anita.k.nandi@gmail.com>
2019-12-09 20:38:41 +0000

    Added LeaveRoomFunction

--- src/GameLogic/IChameleonGame.cs
+++ src/GameLogic/IChameleonGame.cs
@@ -6,5 +6,6 @@
         RoomAndPerson JoinRoom(string roomCode);
         RoomStatus GetRoomStatus(string roomCode, string personId);
         void StartGame(string roomCode, string personId);
+        void LeaveRoom(string roomCode, string personId);
     }
 }
