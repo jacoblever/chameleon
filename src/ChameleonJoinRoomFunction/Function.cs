66551ac8c4e4997cb91e4193a8ca41d5f92ae29c
Jacob Lever <jacob.lever@deliveroo.co.uk>
2019-12-21 12:47:18 +0000

    Add PersonName parameter to JoinRoom lambda function

--- src/ChameleonJoinRoomFunction/Function.cs
+++ src/ChameleonJoinRoomFunction/Function.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using GameLogic;
+using Newtonsoft.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -8,38 +12,47 @@ namespace ChameleonJoinRoomFunction
 {
     public class Function
     {
-        // TODO: Make this a lambda proxy like the others
-        public Person FunctionHandler(JoinRoomRequest input, ILambdaContext context)
+        public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
             try
             {
                 var chameleonGame = ChameleonGame.Create();
-                var roomAndPerson = input.RoomCode == null
-                    ? chameleonGame.CreateRoom("Jacob")
-                    : chameleonGame.JoinRoom(input.RoomCode, "Jacob");
-                return new Person(roomAndPerson.RoomCode, roomAndPerson.PersonId);
+                var person = new RoomJoiner(chameleonGame)
+                    .Join(request.Body);
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 200,
+                    Body = JsonConvert.SerializeObject(person),
+                    Headers = new Dictionary<string, string>
+                    {
+                        { "Access-Control-Allow-Origin", "*" },
+                    },
+                };
             }
             catch (RoomDoesNotExistException e)
             {
-                return new Person(input.RoomCode, e.Message);
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 404,
+                    Body = e.Message,
+                    Headers = new Dictionary<string, string>
+                    {
+                        { "Access-Control-Allow-Origin", "*" },
+                    },
+                };
+            }
+            catch (Exception e)
+            {
+                return new APIGatewayProxyResponse
+                {
+                    StatusCode = 500,
+                    Body = $"{e.Message}\n{e.StackTrace}",
+                    Headers = new Dictionary<string, string>
+                    {
+                        { "Access-Control-Allow-Origin", "*" },
+                    },
+                };
             }
         }
-    }
-
-    public class JoinRoomRequest
-    {
-        public string RoomCode { get; set; }
-    }
-
-    public class Person
-    {
-        public Person(string roomCode, string personId)
-        {
-            RoomCode = roomCode;
-            PersonId = personId;
-        }
-
-        public string RoomCode { get; }
-        public string PersonId { get; }
     }
 }
