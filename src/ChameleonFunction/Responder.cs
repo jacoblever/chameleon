468231c7f39ade6b2a642cef25e904da4c3f510c
Jacob Lever <jacob.lever@deliveroo.co.uk>
2020-01-04 18:41:39 +0000

    Create Responder class to centralise response generation
    Also turn GetPersonId into an extension method

---
+++ src/ChameleonFunction/Responder.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
+using GameLogic;
+
+namespace ChameleonFunction
+{
+    public class Responder
+    {
+        public APIGatewayProxyResponse Respond(Action<APIGatewayProxyResponse> handler)
+        {
+            var response = new APIGatewayProxyResponse
+            {
+                Headers = new Dictionary<string, string>
+                {
+                    { "Access-Control-Allow-Origin", "*" },
+                },
+            };
+
+            try
+            {
+                handler(response);
+            }
+            catch (RoomDoesNotExistException e)
+            {
+                response.StatusCode = 404;
+                response.Body = e.Message;
+            }
+            catch (PersonNotInRoomException e)
+            {
+                response.StatusCode = 403;
+                response.Body = e.Message;
+            }
+            catch (Exception e)
+            {
+                response.StatusCode = 500;
+                response.Body = $"{e.Message}\n{e.StackTrace}";
+            }
+
+            return response;
+        }
+    }
+}
